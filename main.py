95f6509968de8ea84515b1c97e548145841cacda
aditya <adityaranaworks@gmail.com>
2023-03-04 00:53:55 +0530

    Business logic for user create, authenticate by oauth and update user data by id

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 import psycopg2
@@ -16,69 +17,34 @@ while True:
             print('Database connection failed because of - '+error)
             time.sleep(secs=2)
 
-class AuthenticationModel(BaseModel):
-    auth_id: int
-
 class UserDataModel(BaseModel):
     user_name: str
     user_contact: int
-    auth_id: str
+    oauth: Optional [int]
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+class UserUpdateDataModel(BaseModel):
+    user_name: str
+
 
 @app.post('/createUser')
 def create_user(userDataModel:UserDataModel):
-    cursor.execute("""INSERT INTO users (user_name,user_contact,auth_id) VALUES (%s,%s,%s) RETURNING *""",(userDataModel.user_name,userDataModel.user_contact,userDataModel.auth_id))
+    cursor.execute("""INSERT INTO users (user_name,user_contact,oauth) VALUES (%s,%s,%s) RETURNING *""",(userDataModel.user_name,userDataModel.user_contact,userDataModel.oauth))
     new_user_data = cursor.fetchone()
     conn.commit()
-    return {"message": "New user successfully created","data":new_user_data}
+    return {"message": "New user successfully created!","data":new_user_data}
 
-@app.get("/getUser/{auth_id}")
-def get_user_by_id(auth_id:int):
-    cursor.execute("""SELECT * FROM users WHERE auth_id = 1""")
-    fetched_user_data = cursor.fetchone()
-    print(fetched_user_data)
-    return {"message": "User successfully found","data":fetched_user_data}
+@app.get("/authenticateUser/{oauth}")
+def fetched_user_data(oauth:int):
+    cursor.execute("""SELECT * FROM users WHERE oauth = %s""",(str(oauth),))
+    updated_user_data = cursor.fetchone()
+    return {"message": "User exsists!","data":updated_user_data}
 
-
-@app.get("/getAllUser")
-def root():
-    cursor.execute("""SELECT * FROM users""")
-    users = cursor.fetchall()
-    return {"data": users}
-
-@app.post('/authenticateUser')
-def authenticate_user(authenticationModel:AuthenticationModel):
-    print(authenticationModel)
-    return {"message": "This API will authenticate user and send back user id, user contact, user membership type"}
-
-@app.post('/editUserProfile')
-def root():
+@app.post('/updateUserData/{user_id}')
+def edit_user_data(user_id:int,userUpdateDataModel:UserUpdateDataModel):
+    cursor.execute("""UPDATE users SET user_name = %s WHERE user_id = %s RETURNING *""",(userUpdateDataModel.user_name,str(user_id)))
+    updated_user_data = cursor.fetchone()
+    conn.commit()
     return {"message": "This API will help user to edit user profile"}
 
-@app.get('/userCarWashHistory')
-def root():
-    return {"message": "This API will give user car wash history"}
 
-@app.post('/bookCarWash')
-def root():
-    return {"message": "This API will book a single car wash"}
-
-@app.post('/bookCarWashSubscriptionWise')
-def root():
-    return {"message": "This API will book a car wash in subscription mode"}
-
-@app.get('/fetchNotification')
-def root():
-    return {"message": "This API will give user car wash history"}
-
-@app.post('/addNewAddress')
-def root():
-    return {"message": "This API will book a single car wash"}
-
-@app.get('/carWashedDetails')
-def root():
-    return {"message": "This API will give particular car washs details"}
 
