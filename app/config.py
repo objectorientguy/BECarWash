c6ef2e11255f0e18295e1349855cfe10717d9875
Aditi-005 <aditi.diwan005@gmail.com>
2023-05-05 16:41:13 +0530

    Fix Deployment connection issue

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url = 'postgres:Mun1chad$@localhost:5432/carwash'
+    db_url = 'carwashdevs:YbaPElydvXjLYoRzdVD23n91u0q3196W@dpg-cgbu2d5269v4ictdp800-a/carwashdb'
 
     class Config:
         env_file = ".env"
