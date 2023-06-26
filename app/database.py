f7c73db231c74c4158fe3828d34243e2856a6d85
Aditi-005 <aditi.diwan005@gmail.com>
2023-06-26 18:18:42 +0530

    Fix Database url

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://carwashdb_gv4e_user:8sv7IryuQE7R8yDfTNSHF97HqLATIgJD@dpg-cicnvmiip7vnjjl9ecmg-a.singapore-postgres.render.com/carwashdb_gv4e'
+SQLALCHEMY_DATABASE_URL = 'postgresql://carwashdb_gv4e_user:8sv7IryuQE7R8yDfTNSHF97HqLATIgJD@dpg-cicnvmiip7vnjjl9ecmg-a.singapore-postgres.render.com/carwashdb_gv4e'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
