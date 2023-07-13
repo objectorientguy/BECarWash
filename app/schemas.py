5e9b63f0a9efa6aa7efc240cde17e8f4f8becdb7
Aditi-005 <aditi.diwan005@gmail.com>
2023-07-13 17:13:22 +0530

    Implement Car Wash Centers API.

--- app/schemas.py
+++ app/schemas.py
@@ -19,7 +19,6 @@ class GetUserData(BaseModel):
     customer_name: Optional[str]
     customer_birthdate: Optional[date]
     is_new_customer: Optional[bool]
-    
 
     class Config:
         orm_mode = True
@@ -81,6 +80,7 @@ class Bookings(BaseModel):
     class Config:
         orm_mode = True
 
+
 class EditBookings(BaseModel):
     user_contact: int
     address_id: int
@@ -93,6 +93,7 @@ class EditBookings(BaseModel):
 
     class Config:
         orm_mode = True
+
 
 class GetBooking(Bookings):
     customer: GetUserData
@@ -118,6 +119,78 @@ class GetBookingUser(Bookings):
 class GetUserBookings(BaseModel):
     status: int
     data: List[GetBookingUser]
+    message: str
+
+    class Config:
+        orm_mode = True
+
+
+class Center(BaseModel):
+    center_id: Optional[int]
+    center_name: str
+    center_address: str
+    center_ratings: int
+    center_details: str
+
+    class Config:
+        orm_mode = True
+
+
+class EditCenter(BaseModel):
+    center_name: str
+    center_address: str
+    center_ratings: int
+    center_details: str
+
+    class Config:
+        orm_mode = True
+
+
+class AllCenters(BaseModel):
+    status: int
+    data: List[Center]
+    message: str
+
+    class Config:
+        orm_mode = True
+
+
+class AddServices(BaseModel):
+    service_id: Optional[int]
+    center_id: int
+    service_title: str
+    service_cost: int
+    service_discount: int
+    service_details: str
+
+    class Config:
+        orm_mode = True
+
+
+class Services(BaseModel):
+    service_id: Optional[int]
+    service_title: str
+    service_cost: int
+    service_discount: int
+    service_details: str
+
+    class Config:
+        orm_mode = True
+
+
+class EditServices(BaseModel):
+    service_title: str
+    service_cost: int
+    service_discount: int
+    service_details: str
+
+    class Config:
+        orm_mode = True
+
+
+class AllCenterServices(BaseModel):
+    status: int
+    data: List[Services]
     message: str
 
     class Config:
